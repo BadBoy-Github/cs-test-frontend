5c2532e2da4c1faab90f709e835494c4e1a79eb9
BadBoy-Github <elayabarathiedison@gmail.com>
2026-06-27 10:21:04 +0530

    config changed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://cs-test-backend.vercel.app',
         changeOrigin: true,
         secure: true,
       },
